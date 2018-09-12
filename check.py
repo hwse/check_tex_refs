ab58dd0e0eefb503d7d0ac5a36fc3914ea2bfca5
hwse <hagen.weisse@stud.htwk-leipzig.de>
2018-09-12 23:01:56 +0200

    refactored script to use default dict and show line nrs

--- check.py
+++ check.py
@@ -1,43 +1,18 @@
 import re
 import argparse
 import sys
+import collections
 
 class LabelCount:
-    def __init__(self, labels=0, refs=0):
-        self.labels = labels
-        self.refs = refs
-
-    def inc_labels(self):
-        self.labels += 1
-
-    def inc_refs(self):
-        self.refs += 1
-
-
-class LabelCounter:
     def __init__(self):
-        self.counts = dict() # dict[id -> LabelCount]
+        self.label_nrs = list()
+        self.ref_nrs = list()
 
-    def label_found(self, id):
-        if id in self.counts:
-            self.counts[id].inc_labels()
-        else:
-            self.counts[id] = LabelCount(labels=1)
+    def label_found(self, line_nr):
+        self.label_nrs.append(line_nr)
 
-    def ref_found(self, id):
-        if id in self.counts:
-            self.counts[id].inc_refs()
-        else:
-            self.counts[id] = LabelCount(refs=1)
-
-    def print_result(self):
-        for id, count in self.counts.items():
-            if count.labels == 0 and count.refs == 0:
-                print('ERRRRROOORR')
-            if count.labels == 0:
-                print('ref to {} has no matching label'.format(id))
-            if count.refs == 0:
-                print('label {} was never referenced'.format(id))
+    def ref_found(self, line_nr):
+        self.ref_nrs.append(line_nr)
 
 def first(it):
     for el in it:
@@ -46,22 +21,28 @@ def first(it):
 
 def check_missing(file_name):
     with open(file_name, 'r') as file:
-        lc = LabelCounter()
+        counters = collections.defaultdict(lambda: LabelCount())
         label_re = r'\\label{(\w+)}|label\s*=\s*{?(\w+)}?'
         ref_re = r'\\ref{(\w+)}'
-        for nr, line in enumerate(file.readlines()):
-            if '%' in line:
-                i = line.find('%')
-                line = line[:i]
-            # print(line)
-            for label in map(first, re.findall(label_re, line)):
-                # print(label)
-                lc.label_found(label)
-            for ref in re.findall(ref_re, line):
-                # print(ref)
-                lc.ref_found(ref)
-        lc.print_result()
         
+        for nr, line in enumerate(file.readlines()):
+            # ignore comments
+            if '%' in line: # TODO: only match on comments
+                i = line.find('%') 
+                line = line[:i]
+
+            for id in map(first, re.findall(label_re, line)):
+                counters[id].label_found(nr)
+            for id in re.findall(ref_re, line):
+                counters[id].ref_found(nr)
+        print_errors(counters)
+
+def print_errors(counters_dict):
+    for id, count in counters_dict.items():
+        if len(count.label_nrs) == 0:
+            print('ref to {} has no matching label (lines: {})'.format(id, ','.join(map(str, count.label_nrs))))
+        if len(count.ref_nrs) == 0:
+            print('label {} was never referenced (lines: {})'.format(id, ','.join(map(str, count.label_nrs))))
 
 def main():
     parser = argparse.ArgumentParser(description='Check a tex file for missing references to labels')
